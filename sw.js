74e0b0822374961a8005bbb1f69edaa26b7ec728
sakaSORA <g.saka.g@gmail.com>
2025-12-23 08:26:13 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'kioku-no-kanata-v1';
+const urlsToCache = [
+  './',
+  './memo.html',
+  './manifest.json'
+];
+
+// インストール時に指定したファイルをキャッシュに保存
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// ネットワークリクエスト時にキャッシュがあればそれを返し、なければ通信する
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
