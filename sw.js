631202a7a8352a4cac1e52fa671d306b3e812f9a
sakaSORA <g.saka.g@gmail.com>
2025-12-24 17:07:47 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,22 +1,25 @@
-const CACHE_NAME = 'check-app-v2';
+const CACHE_NAME = 'kioku-v12-pro';
 const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+    './',
+    './memo.html',
+    './manifest.json',
+    './icon-192.png',
+    './icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request).catch(() => {
+                // キャッシュが見つからず、オフラインの場合はmemo.htmlを返す
+                return caches.match('./memo.html');
+            });
+        })
+    );
 });
